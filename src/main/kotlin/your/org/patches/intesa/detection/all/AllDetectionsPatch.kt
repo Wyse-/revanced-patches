e60a75a344e5d9ea5e3abb955931989444c1f0d1
Wyse- <5454041+Wyse-@users.noreply.github.com>
2024-01-29 00:32:18 +0100

    fix: add missing imports to intesa patch

--- src/main/kotlin/your/org/patches/intesa/detection/all/AllDetectionsPatch.kt
+++ src/main/kotlin/your/org/patches/intesa/detection/all/AllDetectionsPatch.kt
@@ -4,6 +4,8 @@ import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
+import app.revanced.util.exception
 
 @Patch(
     name = "Remove all detections",
