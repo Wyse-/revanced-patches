2cd348197105a70b4e2bd155f2a7171bb3cb54b4
Wyse- <5454041+Wyse-@users.noreply.github.com>
2024-06-14 19:25:59 +0200

    fix: corrected isybank fingerprints class names

--- src/main/kotlin/your/org/patches/isybank/detection/all/fingerprints/DexguardFingerprintB.kt
+++ src/main/kotlin/your/org/patches/isybank/detection/all/fingerprints/DexguardFingerprintB.kt
@@ -2,7 +2,7 @@ package your.org.patches.example
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
-internal object AllDetectionsFingerprint : MethodFingerprint(
+internal object DexguardFingerprintB : MethodFingerprint(
     customFingerprint = { methodDef, _ ->
         methodDef.definingClass.endsWith("Ljb/a$a;") && methodDef.name == "b"
     }
--- src/main/kotlin/your/org/patches/isybank/detection/all/fingerprints/TamperCheckFingerprintB.kt
+++ src/main/kotlin/your/org/patches/isybank/detection/all/fingerprints/TamperCheckFingerprintB.kt
@@ -2,7 +2,7 @@ package your.org.patches.example
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
-internal object AllDetectionsFingerprint : MethodFingerprint(
+internal object TamperCheckFingerprintB : MethodFingerprint(
     customFingerprint = { methodDef, _ ->
         methodDef.definingClass.endsWith("TamperCheck;") && methodDef.name == "b"
     }
