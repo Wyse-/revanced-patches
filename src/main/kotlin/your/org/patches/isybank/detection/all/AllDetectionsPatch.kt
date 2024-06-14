ead6b50ed72e3606f17d907cd1d5a14899e9495e
Wyse- <5454041+Wyse-@users.noreply.github.com>
2024-06-14 19:52:58 +0200

    fix: added isybank patch imports

--- src/main/kotlin/your/org/patches/isybank/detection/all/AllDetectionsPatch.kt
+++ src/main/kotlin/your/org/patches/isybank/detection/all/AllDetectionsPatch.kt
@@ -1,10 +1,11 @@
 package wyse.patches.isybank
 
+import app.revanced.util.exception
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patcher.extensions.InstructionExtensions.replaceInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.PatchException
 
 @Patch(
