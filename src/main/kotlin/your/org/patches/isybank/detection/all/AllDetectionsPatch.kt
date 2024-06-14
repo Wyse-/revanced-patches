a735066ffb9f840777c4043c3a06ee5c914ba9d2
Wyse- <5454041+Wyse-@users.noreply.github.com>
2024-06-14 15:49:42 +0200

    fix: split isybank patch to its own, added isybank patching fingerprints

--- src/main/kotlin/your/org/patches/isybank/detection/all/AllDetectionsPatch.kt
+++ src/main/kotlin/your/org/patches/isybank/detection/all/AllDetectionsPatch.kt
@@ -10,16 +10,15 @@ import app.revanced.patcher.patch.PatchException
 @Patch(
     name = "Remove all detections",
     description = "Removes all checks (certificate, debug key signature, emulator, installation origin).",
-    compatiblePackages = [CompatiblePackage("com.latuabancaperandroid")]
+    compatiblePackages = [CompatiblePackage("com.intesasanpaolo.isybank.mobile")]
 )
 @Suppress("unused")
 object AllDetectionsPatch : BytecodePatch(
-    setOf(AllDetectionsFingerprint)
+    setOf(DexguardFingerprintA, DexguardFingerprintB, TamperCheckFingerprintA, TamperCheckFingerprintB)
 ) {
-    override fun execute(context: BytecodeContext) = AllDetectionsFingerprint.result?.mutableMethod?.replaceInstructions(
-        0,
-        """
-            return-void
-        """
-    ) ?: throw PatchException("Failed to resolve ${this.javaClass.simpleName}")
+    override fun execute(context: BytecodeContext) =
+        setOf(DexguardFingerprintA, DexguardFingerprintB, TamperCheckFingerprintA, TamperCheckFingerprintB).forEach { fingerprint ->
+            fingerprint.result?.mutableMethod?.replaceInstruction(0, "return-void")
+                ?: throw fingerprint.exception
+        }
 }
---
+++ src/main/kotlin/your/org/patches/isybank/detection/all/fingerprints/TamperCheckFingerprintA.kt
@@ -0,0 +1,9 @@
+package your.org.patches.example
+
+import app.revanced.patcher.fingerprint.MethodFingerprint
+
+internal object AllDetectionsFingerprint : MethodFingerprint(
+    customFingerprint = { methodDef, _ ->
+        methodDef.definingClass.endsWith("TamperCheck;") && methodDef.name == "a"
+    }
+)
